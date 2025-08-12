134d769e8562beefcf5722305a6dd20c62df4b21
onthatile <onthatilenthutang20@gmail.com>
2025-08-12 09:21:25 +0200

    new sound

--- script.js
+++ script.js
@@ -2,15 +2,18 @@
 // Object-Oriented Programming (OOP)
 // ____________________________
 class Pet {
-    constructor(name, sound, image) {
+    constructor(name, soundText, image, soundUrl) {
       this.name = name;
-      this.sound = sound;
+      this.soundText = soundText;
       this.image = image;
+      this.soundUrl = new Audio(soundUrl)
       this.playCount = 0;
     }
   
     speak() {
-      alert(this.sound);
+      alert(this.soundText);
+       this.soundUrl.currentTime = 0; // Ensure restart each click
+       this.soundUrl.play();
     }
   
     play() {
@@ -29,9 +32,13 @@ class Pet {
   // ____________________________
   function choosePet(type) {
     if (type === 'dog') {
-      currentPet = new Pet('Dog', 'Woof! 🐶', 'dog.jpg');
-    } else {
-      currentPet = new Pet('Cat', 'Meow! 🐱', 'cat.jpg');
+      currentPet = new Pet('Dog', 'Woof! 🐶', 'dog.jpg', 'dog-bark-effect-382711.mp3',);
+    } else if (type === 'cat') {
+      currentPet = new Pet('Cat', 'Meow!', 'cat.jpg' , 'cat-meow-sound-383823.mp3',);
+    } else if (type === 'snake') {
+      currentPet = new Pet('snake', 'hissss!', 'pit-viper-snake.webp', 'snake-hiss-95241.mp3',);
+    } else if (type === 'rabbit') {
+      currentPet = new Pet('rabbit', 'twick', 'rabbit.jpeg', 'rabbit-sounds-358172.mp3');
     }
   
     document.getElementById('petImage').src = currentPet.image;
@@ -60,10 +67,17 @@ class Pet {
   // Function + Loop: Fun pet actions
   // ____________________________
   function showFunActions(petName) {
-    const actions = petName === 'Dog'
-      ? ['Bark', 'Wag Tail', 'Fetch Ball']
-      : ['Meow', 'Purr', 'Chase Mouse'];
+    let actions = [];
   
+    if (petName === 'Dog') {
+      actions = ['Bark', 'Wag Tail', 'Fetch Ball'];
+    } else if (petName === 'Cat') {
+      actions = ['Meow', 'Purr', 'Chase Mouse'];
+    } else if (petName === 'snake') {
+      actions = ['Hissing', 'Sneaky Slide', 'Tongue Detector'];
+    } else if (petName === 'rabbit') {
+      actions = ['twitch', 'hop around', 'munch a carrot'];
+    }
     const actionBox = document.getElementById('actions');
     actionBox.innerHTML = '<strong>Fun Actions:</strong><br>';
   
